2151d4bbc62afaf2bcffe9f199c9d797f08795cc
TomHenderson21 <tomcallumhenderson@gmail.com>
2026-04-30 18:37:58 +0100

    added crouch trap to level 3 and moved damage scripts into the playerhealth script

--- TSE/Assets/Scripts/PlayerHealth.cs
+++ TSE/Assets/Scripts/PlayerHealth.cs
@@ -7,13 +7,33 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Spike"))
+        if(collision.gameObject.CompareTag("Spike"))    //check if player collides with the spikes
         {
             health -= 100;
 
             if(health <= 0)
             {
                 Die();
+            }
+        }
+        else if(collision.gameObject.CompareTag("FalseDoor"))   //check if the player collides with the false door
+        {
+            health -= 100;
+            {
+                if (health <= 0)
+                {
+                    Die();
+                }
+            }
+        }
+        else if (collision.gameObject.CompareTag("CrouchTrap")) //check if the player collides with the crouch trap
+        {
+            health -= 25;
+            {
+                if (health <= 0)
+                {
+                    Die();
+                }
             }
         }
     }
