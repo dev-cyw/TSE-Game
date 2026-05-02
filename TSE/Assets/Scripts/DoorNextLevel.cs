a13e02e048bee323a238da813c3d085502dd61ba
TomHenderson21 <tomcallumhenderson@gmail.com>
2026-05-02 21:55:17 +0100

    added death barrier for if user falls off map, added a transition between, edited spikes on level 3 to make functional

--- TSE/Assets/Scripts/DoorNextLevel.cs
+++ TSE/Assets/Scripts/DoorNextLevel.cs
@@ -1,19 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class DoorNextLevel : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)  
-    { 
-        if(collision.gameObject.CompareTag("Player"))   //check if the player collides with the door
+    [SerializeField] Animator transition;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))   //check if the player collides with the door
         {
             NextLevel();    //if player does load the next level
         }
     }
-
     private void NextLevel()
     {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   //load the next level in the build
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("End");   //play the transition animation
+        yield return new WaitForSeconds(1);  //wait for the animation to finish
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   //load the next level in the build
+        transition.SetTrigger("Start");     //play the end transition animation
     }
 }
 
